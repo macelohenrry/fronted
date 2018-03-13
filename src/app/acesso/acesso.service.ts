e5f1dea8a6d295d538ea5698f07a988cd72b9ee2
Marcelo Cabo <marcelohenrr@hotmail.com>
2018-03-13 11:28:19 -0300

    Ainda

--- src/app/acesso/acesso.service.ts
+++ src/app/acesso/acesso.service.ts
@@ -10,16 +10,20 @@ export class AcessoService {
   public token: string;
   private url = "http://localhost:8080";
 
-
+  options = new RequestOptions({
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  });
   constructor(private http: Http, private formBuilder: FormBuilder) { 
 
   }
 
   onSubmit(formularioLogin: FormGroup) {
-    this.http.post(`${this.url}/login`, JSON.stringify(formularioLogin.value))
+    this.http.post(`${this.url}/login`, JSON.stringify(formularioLogin.value), this.options)
       .map((response: Response) => {
         
-        console.log(response);
+        console.log(response.json().token);
       })
       .subscribe(dados => console.log(dados));
       
